32bff40a55ac4bc7c85e5bf03275b3e47741b5e7
Supawadee Yeamput <supawadee.yeamput@scb.co.th>
2023-10-12 10:35:48 +0700

    Initial commit

---
+++ src/main/java/com/example/assingment_be/AssingmentBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.assingment_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssingmentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssingmentBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assingment_be/AssingmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assingment_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssingmentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
